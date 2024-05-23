8058f162b02753ec718046c36c55af189f64f19b
Mohammed Sadeq Saeed <mohammed.saeed@basworld.com>
2024-05-23 17:00:09 +0200

    build: added laravel rector

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -19,4 +20,6 @@ return RectorConfig::configure()
     // ->withPhpSets()
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
+    ]) ->withSets([
+        LaravelSetList::LARAVEL_110,
     ]);
